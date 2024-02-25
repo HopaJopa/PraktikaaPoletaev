59dd9af5dce9b832f170d1d427529f6427af8d5e
HopaJopa <danusha.com@mail.ru>
2024-02-25 19:35:36 +0400

    last commit

---
+++ MagazTurizmaPoletaev/Migrations/20240219105742_hyu.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagazTurizmaPoletaev.Migrations
+{
+    /// <inheritdoc />
+    public partial class hyu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nazv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    kratkOpis = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    polnOpis = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    img = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
